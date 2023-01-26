68edf7d8779127d8ee69ff577a94e923e670e794
blessdamy <blessdamycode21@gmail.com>
2023-01-26 14:28:54 +0100

    intalled React Router and Icon

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Testing from "./Components/Testing";
+
+function App() {
+  return (
+    <>
+         <div className='text-center'>
+             <Testing />
+ 
+         </div>
+    </>
+   )
+}
+
+export default App
---
+++ src/Components/Testing.jsx
@@ -0,0 +1,12 @@
+function Testing() {
+    return (
+      <>
+          <div className=" bg-green-200">
+                <h1 className="text-2xl">This Ecommerce Application will be built by Team Intel</h1>
+          </div>
+      </>
+    );
+  }
+  
+  export default Testing;
+  
