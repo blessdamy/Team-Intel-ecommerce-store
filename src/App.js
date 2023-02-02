fee027835a9aa4789cc25737f102407cee636f9a
ibrahim dotto jemila <jemiladotto@gmail.com>
2023-02-02 17:39:44 +0100

    Navbar and footer first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Navbar from "./Components/Navbar";
 import Testing from "./Components/Testing";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
          <div className='text-center'>
+             <Navbar />
              <Testing />
+             <Footer />
  
          </div>
     </>
